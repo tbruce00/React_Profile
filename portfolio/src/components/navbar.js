6aa847072f5c593be02a49040b966e203ea6579f
Trevor Bruce <tbnyk03@gmail.com>
2021-07-19 20:26:28 -0600

    Add social media links to navbar

--- portfolio/src/components/navbar.js
+++ portfolio/src/components/navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaAsterisk } from 'react-icons/fa';
 // import nav style sheet
 import { GrInstagram } from "react-icons/gr";
 import { GrFacebook } from "react-icons/gr";
@@ -7,7 +8,47 @@ import { GrTwitter } from "react-icons/gr";
 
 function Navbar({ currentPage, handlePageChange}) {
     return (
-        <div className= "nav navBar">
+        <div className= "navBar">
+        <span className="navIcon">
+        <FaAsterisk />
+        </span>
+        <ul className="nav navLinks justify-content-center">
+            <li className="nav-item">
+                <a href="#about" onClick={() => handlePageChange("About")}
+                className={currentPage === "About" ? "nav-link-active" : "nav-link"}>
+                
+                about
+                
+                </a>
+            </li>
+            <li className="nav-item">
+                <a href="#portfolio" onClick={() => handlePageChange("Projects")}
+                className={currentPage === "Projects" ? "nav-link-active" : "nav-link"}>
+                
+                projects    
+                
+                </a>
+            </li>
+            <li className="nav-item">
+                <a href="#contact" onClick={() => handlePageChange("Contact")}
+                className={currentPage === "Contact" ? "nav-link-active" : "nav-link"}>
+
+                contact
+                
+                </a>
+            </li>
+        </ul>
+        <div className="socialMedia">
+            <span>
+                <GrInstagram />
+            </span>
+            <span>
+                <GrFacebook />
+            </span>
+            <span>
+                <GrTwitter />
+            </span>
+        </div>
 
         </div>
     )
--- portfolio/src/components/pages/contact.js
+++ portfolio/src/components/pages/contact.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import Form from '../form';
 
 export default function Contact() {
     return (
-        <div className="contact">
-
-
+        <div className="mx-auto mt-auto p-auto">
+        <h1 className="text-center">Contact Me</h1>
+        <Form />
         </div>
     );
 }
